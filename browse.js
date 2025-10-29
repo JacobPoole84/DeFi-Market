0d7795bf0ef2e23f17570c0c52890ced134844c5
JacobPoole84 <jacobpoole84@gmail.com>
2025-10-29 10:33:39 -0400

    update

--- browse.js
+++ browse.js
@@ -1,13 +1,12 @@
 
 
-const coinContainerEl = document.querySelector('.coin__container')
 
 async function main() {
     const coin = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
     const coinData = await coin.json()
+    const coinContainerEl = document.querySelector('.coin__container')
     coinContainerEl.innerHTML = coinData.map((coin) => coinHTML(coin)).join('')
 }
-
 main()
 
 function coinHTML(coin) {
